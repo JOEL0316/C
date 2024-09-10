2f6cbc5e555a86d031a55a07a3ee4c56e4e4bae1
JOEL0316 <90368526+JOEL0316@users.noreply.github.com>
2024-09-09 22:09:27 -0600

    Add files via upload

---
+++ Apuntadores1.c
@@ -0,0 +1,15 @@
+//Tipo de dato *()identificador(nombre)
+# include <stdio.h>
+
+int main(){
+    int a=20;
+    int b;
+    int suma;
+
+printf("La direccion de a es: %d\n",&a);
+printf("La direccion de a es: %p\n",&a);
+printf("La direccion de b es: %d\n",&b);
+printf("La direccion de b es: %p\n",&b);
+
+    return 0;
+}
